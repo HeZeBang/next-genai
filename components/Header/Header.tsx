5f6c6406ced34ad62c0a6f4844ff29903157cdd9
zambar <zambar@163.com>
2025-02-16 23:41:25 +0800

    feat: removed header nav

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,11 +11,11 @@ import { useTheme } from '../Themes'
 
 export const Header = () => {
   const { theme, setTheme } = useTheme()
-  const [, setShow] = useState(false)
+  // const [, setShow] = useState(false)
 
-  const toggleNavBar = useCallback(() => {
-    setShow((state) => !state)
-  }, [])
+  // const toggleNavBar = useCallback(() => {
+  //   setShow((state) => !state)
+  // }, [])
 
   return (
     <header
@@ -54,7 +54,7 @@ export const Header = () => {
             </Select.Content>
           </Select.Root>
         </Flex>
-        <Tooltip content="Navigation">
+        {/* <Tooltip content="Navigation">
           <IconButton
             size="3"
             variant="ghost"
@@ -64,7 +64,7 @@ export const Header = () => {
           >
             <HamburgerMenuIcon width="16" height="16" />
           </IconButton>
-        </Tooltip>
+        </Tooltip> */}
       </Flex>
     </header>
   )
