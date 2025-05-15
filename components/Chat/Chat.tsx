23d59125c03f447dc14315daadb40e83e8197336
zambar <zambar@shanghaitech.edu.cn>
2025-05-16 03:17:17 +0800

    feat: new row ui

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -343,8 +343,8 @@ const Chat = (props: ChatProps, ref: any) => {
           </ScrollArea>
           <div className="px-4 pb-3">
             <Container size="3">
-              <Flex align="end" justify="between" gap="3" className="relative">
-                <div className="rt-TextAreaRoot rt-r-size-1 rt-variant-surface flex-1 rounded-3xl chat-textarea">
+              <Flex align="end" justify="between" gap="3" className="relative border-2 border-gray-400 rounded-3xl focus-within:border-purple-300 focus-within:shadow-lg transition-all" direction="column" >
+                <div className="rt-TextAreaRoot rt-r-size-1 rt-variant-surface flex-1 shadow-none rounded-3xl chat-textarea w-full">
                   <ContentEditable
                     innerRef={textAreaRef}
                     style={{
@@ -369,7 +369,7 @@ const Chat = (props: ChatProps, ref: any) => {
                   />
                   <div className="rt-TextAreaChrome"></div>
                 </div>
-                <Flex gap="3" className="absolute right-0 pr-4 bottom-3 pt">
+                <Flex gap="3" className="pb-2 pr-2">
                   {isLoading && (
                     <Flex
                       width="6"
